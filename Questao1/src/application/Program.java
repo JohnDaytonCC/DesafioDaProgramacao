6023eaedafee7c46526da7650cdd81a69ba57819
JohnDaytonCC_linux <john.dayton.cc@gmail.com>
2022-02-17 17:33:02 -0300

    Reorganização dos comentários do desafio 1 e 2

--- Questao1/src/application/Program.java
+++ Questao1/src/application/Program.java
@@ -5,37 +5,35 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		// Comando para habilitar a entrada de dados via teclado.
-		Scanner input = new Scanner(System.in);
 		
-		// Instruções para informar o usuário a função do programa
+		Scanner input = new Scanner(System.in);					// Comando para habilitar a entrada de dados via teclado.
+		
+																// Instruções para informar o usuário a função do programa
 		System.out.println("Este programa fonecerá a imagem de uma escada de N degraus, onde \nesse valor N será informado pelo usuário. ");
 		System.out.println();
 		
 		System.out.print("Por favor digite um valor inteiro de N: ");
 		int n = input.nextInt();
 		
-		// Espaço para separar a pergunta do resultado.
-		System.out.println();
+		System.out.println();									// Espaço para separar a pergunta do resultado.
 		
-		// Variável utilizada para armazenar os caracteres que representarão os degraus.
-		String degrau = "";
+		String degrau = "";										// Variável utilizada para armazenar os caracteres que representarão os degraus.
 		
-		for(int i = 1; i <= n; i++) {
-			for(int j=1; j<=(n-i); j++) {
-				degrau = degrau + " ";
-			}
-			for(int k=1; k<=i; k++) {
-				degrau = degrau + "*";
-			}
+		for(int i = 1; i <= n; i++) {							// Laço que determina a quantidade de linas ou degraus da escada
 			
-			// Comando para imprimir na tela cada degrau
-			System.out.println(degrau);
+			for(int j=1; j<=(n-i); j++) {						// Laço que determina a quantidade do caractere espaço em cada linha
+				degrau = degrau + " ";							// Inclução da quantidade de vezes que o caractere espaço será utilizado em cada linha
+			}													// Final do laço que determina a quantidade do caractere espaço em cada linha
 			
-			// Limpando a variável
-			degrau = "";
-		}
-		input.close();
+			for(int k=1; k<=i; k++) {							// Laço que determina a quantidade do caractere asterisco em cada linha
+				degrau = degrau + "*";							// Inclução da quantidade de vezes que o caractere asterisco será utilizado em cada linha
+			}													// Final do laço que determina a quantidade do caractere asterisco em cada linha
+			
+			System.out.println(degrau);							// Comando para imprimir na tela cada degrau
+
+			degrau = "";										// Limpando a variável
+		}														// Fim do laço for(int i = 1; i <= n; i++)
+		input.close();											// Encerramento da entrada de dados pelo teclado
 	}
 
-}
+}																// Fim da classe Program
--- Questao2/src/application/Program.java
+++ Questao2/src/application/Program.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		// Comando para habilitar a entrada de dados via teclado pela variável "input".
-		Scanner input = new Scanner(System.in);
+		
+		Scanner input = new Scanner(System.in);								// Comando para habilitar a entrada de dados via teclado pela variável "input".
 
 		System.out.println("Para cadastrar a sua rede social digite, por favor, o seu nome e senha");
 		System.out.print("\nDigite o nome de usuário: ");
-		String nome = input.nextLine();		// Guardando o nome passado pelo teclado em uma variável do tipo String
+		String nome = input.nextLine();										// Guardando o nome passado pelo teclado em uma variável do tipo String
 
 		System.out.println(
 				"\nAgora digite uma senha forte.\nPara ser uma senha forte são necessários os seguintes critérios");
@@ -22,23 +22,23 @@ public class Program {
 
 		
 		System.out.print("\nDigite a senha: ");
-		String senha = input.nextLine();	// Guardando a senha passada pelo teclado em uma variável do tipo String
+		String senha = input.nextLine();									// Guardando a senha passada pelo teclado em uma variável do tipo String
 		
-		boolean flag = true;	// Utilização de um indicador para o controle do laço de repetição "while".
+		boolean flag = true;												// Utilização de um indicador para o controle do laço de repetição "while".
 
-		while (flag) {	// Inicialização do laço while
+		while (flag) {														// Inicialização do laço while
 			
-			if(senha.length() < 6){	// veridica o tamanho da String, caso tenha menos de 6 caracteres, entrará no if.
+			if(senha.length() < 6){											// veridica o tamanho da String, caso tenha menos de 6 caracteres, entrará no if.
 				System.out.print("\nSua senha não é considerada forte.\nPor favor acrescente mais " + String.format("%d", (6 - senha.length())) + " caracteres à sua senha.");
 				System.out.print("\nDigite a senha: ");
-				senha = input.nextLine();	// Guardando a nova senha passada pelo usuário.
-			}else {		// Caso a senha tenha 6 ou mais caractere indicará a mensagem de sucesso.
+				senha = input.nextLine();									// Guardando a nova senha passada pelo usuário.
+			}else {															// Caso a senha tenha 6 ou mais caractere indicará a mensagem de sucesso.
 				System.out.println("\nSua senha foi cadastrada com sucesso!");
-				flag = false;	// Mudando do valor do indicador para sair do laço de repetição e finalizar o programa.
-			}
-		}	// Fim do laço while
+				flag = false;												// Mudando do valor do indicador para sair do laço de repetição e finalizar o programa.
+			}																// Fim do if else
+		}																	// Fim do laço while
 
-		input.close();	// fechamento da variável de entrada de dados.
+		input.close();														// fechamento da variável de entrada de dados.
 	}
 
 }
